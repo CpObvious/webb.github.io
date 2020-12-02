f7e6e2ddf3cc013630b5bd9b32d8306b4e7433ef
CpObvious <getObvious@gmail.com>
2020-12-02 18:15:17 +0300

    jf

---
+++ js/my-script.js
@@ -0,0 +1,6 @@
+let btnDefault = document.querySelector('.btn-default');
+let intro = document.querySelector('.intro');
+
+btnDefault.addEventListener("click", () => { alert('Щелчок по кнопке!'); });
+
+intro.style.border = "5px dashed gold";
